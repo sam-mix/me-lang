d8cb0b5d386110565ecfa015699d17f47c675029
Sam Mix <1045262335@qq.com>
2023-03-21 00:27:22 +0800

    finish test next token 1

---
+++ lexer/lexer.go
@@ -0,0 +1,56 @@
+package lexer
+
+import "me-lang/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case 0:
+		tok = token.Token{Type: token.EOF, Literal: ""}
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(s token.TokenType, b byte) token.Token {
+	return token.Token{Type: s, Literal: string(b)}
+}
--- token/token.go
+++ token/token.go
@@ -4,7 +4,7 @@ type TokenType string
 
 type Token struct {
 	Type    TokenType
-	Literao string
+	Literal string
 }
 
 const (
